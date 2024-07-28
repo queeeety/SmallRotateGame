5d79af9e2e1f254c4f4d84930a47d9adb1e86ce6
Tymofii Bezverkhyi <148719219+queeeety@users.noreply.github.com>
2024-07-28 18:50:43 +0100

    Initial Commit

---
+++ SmallRotateGame/SmallRotateGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmallRotateGameApp.swift
+//  SmallRotateGame
+//
+//  Created by Тимофій Безверхий on 28.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct SmallRotateGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
